882ff81a7bdabc01180d9a216cb77742010072b9
Cristian <christian.dulcey@gmail.com>
2024-08-26 23:32:54 -0500

    update envs name in dockerfile

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,69 +1,73 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
-import { join } from 'path';
-import { SqsModule } from '@ssut/nestjs-sqs';
-import { SQSClient } from '@aws-sdk/client-sqs';
-import { fromEnv } from '@aws-sdk/credential-provider-env';
+import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {MailerModule} from '@nestjs-modules/mailer';
+import {EjsAdapter} from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import {join} from 'path';
+import {SqsModule} from '@ssut/nestjs-sqs';
+import {SQSClient} from '@aws-sdk/client-sqs';
+import {fromEnv} from '@aws-sdk/credential-provider-env';
 
-import { EmailService } from './email.service';
-import { EmailController } from './email.controller';
+import {EmailService} from './email.service';
+import {EmailController} from './email.controller';
 
 const sqsClient = new SQSClient({
-  region: process.env.AWS_REGION,
-  credentials: fromEnv(),
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
 });
 
 @Module({
-  imports: [
-    ConfigModule,
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        transport: {
-          host: configService.get<string>('MAIL_HOST'),
-          port: configService.get<number>('MAIL_PORT'),
-          secure: false,
-          auth: {
-            user: configService.get<string>('MAIL_USER'),
-            pass: configService.get<string>('MAIL_PASS'),
-          },
+    imports: [
+        ConfigModule,
+        MailerModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                transport: {
+                    host: configService.get<string>('MAIL_HOST'),
+                    port: configService.get<number>('MAIL_PORT'),
+                    secure: false,
+                    auth: {
+                        user: configService.get<string>('MAIL_USER'),
+                        pass: configService.get<string>('MAIL_PASS'),
+                    },
+                },
+                defaults: {
+                    from: configService.get<string>('MAIL_FROM'),
+                },
+                template: {
+                    dir: join(__dirname, './assets/templates'),
+                    adapter: new EjsAdapter(),
+                    options: {
+                        strict: true,
+                    },
+                },
+            }),
+            inject: [ConfigService],
+        }),
+        SqsModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                consumers: [
+                    {
+                        name: 'paystreme-notifications',
+                        queueUrl: configService.get<string>('SQS_QUEUE_URL'),
+                        region: configService.get<string>('AWS_REGION'),
+                    },
+                ],
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    controllers: [EmailController],
+    providers: [
+        EmailService,
+        {
+            provide: 'SQS_CLIENT',
+            useValue: sqsClient,
         },
-        defaults: {
-          from: configService.get<string>('MAIL_FROM'),
-        },
-        template: {
-          dir: join(__dirname, './assets/templates'),
-          adapter: new EjsAdapter(),
-          options: {
-            strict: true,
-          },
-        },
-      }),
-      inject: [ConfigService],
-    }),
-    SqsModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        consumers: [
-          {
-            name: 'paystreme-notifications',
-            queueUrl: configService.get<string>('SQS_QUEUE_URL'),
-            region: configService.get<string>('AWS_REGION'),
-          },
-        ],
-      }),
-      inject: [ConfigService],
-    }),
-  ],
-  controllers: [EmailController],
-  providers: [
-    EmailService,
-    {
-      provide: 'SQS_CLIENT',
-      useValue: sqsClient,
-    },
-  ],
+    ],
 })
-export class EmailModule {}
+export class EmailModule {
+}
